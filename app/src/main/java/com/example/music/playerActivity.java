c402b9e69a2d8a473cdf7d33c1a4ae3db874438b
AIS245 <gaishwarya24@gmail.com>
2021-09-14 13:51:57 +0530

    first commit

---
+++ app/src/main/java/com/example/music/playerActivity.java
@@ -0,0 +1,15 @@
+package com.example.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class playerActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
